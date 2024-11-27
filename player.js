03be2dd32de1dccb6d2f318e8b5334f4cc2a0480
Mathilda <huma24ey@student.ju.se>
2024-11-27 12:00:30 +0100

    aim development
    player object now working with aim
    
    >need to fix origin

--- player.js
+++ player.js
@@ -1,11 +1,24 @@
-function setup() {
-    createCanvas(500, 500);
+
+class Player {
+  constructor (x, y) {
+      this.x = x;
+      this.y = y;
 
   }
-  
+  draw() {
+      // translate(250, 275);
+      rect(this.x, this.y, 70, 50);
+  }
+
+}
+
+let player = new Player(0, 0);
+let playerX = player.x;
+let playerY = player.y;
+
 function draw() {
-    background(200);
-    // Translate the origin to the center.
+  clear();
+    // Translate the origin to the center. - needs to be fixed
     translate(250, 250);
   
     // Get the mouse's coordinates relative to the origin.
@@ -17,7 +30,7 @@ function draw() {
   
     // Rotate
     rotate(a);
-  
+    player.draw(-50, -25);
     // Draw the shape
     rect(-30, -5, 60, 10);
   }
