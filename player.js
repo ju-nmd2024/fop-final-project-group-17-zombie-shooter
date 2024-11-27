38cc1957d855e50cb3f2b02ce0062c8cfd4b2a26
pannatheszucs <szpa23at@student.ju.se>
2024-11-27 11:51:27 +0100

    Aiming works
    Character/Player is now rotating towards cursor

--- player.js
+++ player.js
@@ -1,34 +1,23 @@
-export default class Player {
-    constructor (x, y) {
-        this.x = x;
-        this.y = y;
+function setup() {
+    createCanvas(500, 500);
 
-    }
-    draw() {
-        // translate(250, 275);
-        rect(this.x, this.y, 70, 50);
-    }
-
-}
-
-let player = new Player(0, 0);
-let playerX = player.x;
-let playerY = player.y;
-
+  }
+  
 function draw() {
-    clear();
-
-    let playerX = mouseX - 50;
-    let playerY = mouseY - 50;
+    background(200);
+    // Translate the origin to the center.
+    translate(250, 250);
+  
+    // Get the mouse's coordinates relative to the origin.
+    let x = mouseX - 250;
+    let y = mouseY - 250;
   
     // Calculate the angle between the mouse and the origin.
-    let a = atan2(playerX, playerY);
-
+    let a = atan2(y, x);
+  
+    // Rotate
     rotate(a);
-    player.draw();
-    // player.x = mouseX-250;
-    // player.y = mouseY-275;
-    
-
-
-}
+  
+    // Draw the shape
+    rect(-30, -5, 60, 10);
+  }
