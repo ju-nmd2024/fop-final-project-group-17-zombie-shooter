d99b002acc2452ba0695a2746c213ae973fdde15
Mathilda <huma24ey@student.ju.se>
2024-12-03 17:45:20 +0100

    zombie "behaviour"

--- spawnpointTopLeft.js
+++ spawnpointTopLeft.js
@@ -1,9 +1,17 @@
 export default class SpawnpointTopLeft {
-    constructor (x, y, gridWidth, gridHeight) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        const spawnpointTopLeft = (x + 2, y + 2);
-        this.gridWidth = gridWidth;
-        this.gridHeight = gridHeight;
     }
+
+    getSpawnPoint() {
+        return { x: this.x, y: this.y };
+    }
+    // constructor (x, y, gridWidth, gridHeight) {
+    //     this.x = x;
+    //     this.y = y;
+    //     const spawnpointTopLeft = (x + 2, y + 2);
+    //     this.gridWidth = gridWidth;
+    //     this.gridHeight = gridHeight;
+    // }
 }
--- zombie.js
+++ zombie.js
@@ -4,14 +4,49 @@ import PathBottomLeft from "./pathBottomLeft";
 import PathBottomRight from "./pathBottomRight";
 
 class Zombie {
-    constructor (x, y, gridWidth, gridLength) {
-        this.x = x;
-        this.y = y;
-        this.zombieSpeed = 2;
-        thiscurrentWaypointIndex = 0;
-        this.gridWidth = gridWidth;
-        this.gridLenth = gridLength;
+
+    constructor(spawnPoint, path, speed = 2) {
+        this.x = spawnPoint.x;
+        this.y = spawnPoint.y;
+        this.path = path;
+        this.speed = speed;
+        this.currentWaypointIndex = 0;
     }
+
+    moveTowards(target) {
+        const dx = target.x - this.x;
+        const dy = target.y - this.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance > this.speed) {
+            this.x += (dx / distance) * this.speed;
+            this.y += (dy / distance) * this.speed;
+        } else {
+            this.x = target.x;
+            this.y = target.y;
+            this.currentWaypointIndex++;
+        }
+    }
+
+    update() {
+        if (this.currentWaypointIndex < this.path.length) {
+            this.moveTowards(this.path[this.currentWaypointIndex]);
+        }
+    }
+
+    draw() {
+        fill(150, 50, 50);
+        ellipse(this.x, this.y, 20, 20); // Draw as a circle for simplicity
+    }
+
+    // constructor (x, y, gridWidth, gridLength) {
+    //     this.x = x;
+    //     this.y = y;
+    //     this.zombieSpeed = 2;
+    //     thiscurrentWaypointIndex = 0;
+    //     this.gridWidth = gridWidth;
+    //     this.gridLenth = gridLength;
+    // }
 
     // moveTowards(target) {
     //     const dx = target.x - this.x;
@@ -35,10 +70,10 @@ class Zombie {
     //     }
     //   }
 
-    draw() {
-        fill (50, 150, 100);
-        rect(this.x, this.y, 30, 30);
-    }
+    // draw() {
+    //     fill (50, 150, 100);
+    //     rect(this.x, this.y, 30, 30);
+    // }
 }
 
 let zombie = new Zombie(100, 100);
