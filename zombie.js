c134d53bf118e3fc23db20268633d0feb4618ec6
Mathilda <huma24ey@student.ju.se>
2024-12-04 22:48:04 +0100

    zombie.js cleanup

--- zombie.js
+++ zombie.js
@@ -56,90 +56,10 @@ export default class Zombie {
         //     this.pathIndex++;
         // }
 
-        //connect index thing to path thing...
-        //zombin att gå mot punk, och
-
-        //we need to add zombie collisison, zombie spawn rate
 
 
-
-
-
-
-
-
-
-    // moveTowards(target) {
-    //     const dx = target.x - this.x;
-    //     const dy = target.y - this.y;
-    //     const distance = Math.sqrt(dx * dx + dy * dy);
-
-    //     if (distance > this.speed) {
-    //         this.x += (dx / distance) * this.speed;
-    //         this.y += (dy / distance) * this.speed;
-    //     } else {
-    //         this.x = target.x;
-    //         this.y = target.y;
-    //         this.currentWaypointIndex++;
-    //     }
-    // }
-
-    // update() {
-    //     if (this.currentWaypointIndex < this.spawnPoint.path.length) {
-    //         this.moveTowards(this.path[this.currentWaypointIndex]);
-    //     }
-    // }
-
-    // constructor (x, y, gridWidth, gridLength) {
-    //     this.x = x;
-    //     this.y = y;
-    //     this.zombieSpeed = 2;
-    //     thiscurrentWaypointIndex = 0;
-    //     this.gridWidth = gridWidth;
-    //     this.gridLenth = gridLength;
-    // }
-
-    // moveTowards(target) {
-    //     const dx = target.x - this.x;
-    //     const dy = target.y - this.y;
-    //     const distance = Math.sqrt(dx * dx + dy * dy);
-
-    //     if (distance > this.speed) {
-    //         this.x += (dx / distance) * this.speed;
-    //         this.y += (dy / distance) * this.speed;
-    //       } else {
-    //         // Snap to the waypoint if close enough
-    //         this.x = target.x;
-    //         this.y = target.y;
-    //         this.currentWaypointIndex++; // Move to the next waypoint
-    //     }
-    // }
-    
-    // update() {
-    //     if (this.currentWaypointIndex < path.length) {
-    //       this.moveTowards(path[this.currentWaypointIndex]);
-    //     }
-    //   }
-
-    // draw() {
-    //     fill (50, 150, 100);
-    //     rect(this.x, this.y, 30, 30);
-    // }
 }
 
-// let zombie = new Zombie(100, 100);
 
 
-
-// function draw() {
-//     clear();
-//     zombie.draw();
-//         zombie.x++;
-//         zombie.y++;
-
-// }
-
-// function draw() {
-//     zombie.draw();
-// }
-
+//we need to add zombie collisison, zombie spawn rate
